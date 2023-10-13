1feefacafd41b9ea1d4dc97ffdebbb228bde6270
Anonymanpro <135943382+daitign@users.noreply.github.com>
2023-10-13 11:20:06 +0800

    directory name text color

--- resources/views/archives/common/directory.blade.php
+++ resources/views/archives/common/directory.blade.php
@@ -5,7 +5,7 @@
         data-bs-toggle="dropdown" aria-expanded="false" 
         data-route="{{ route($route ?? 'archives-page') }}?directory={{ $directory->id }}" style="border:none">
             <img src="{{ Storage::url('assets/folder.png') }}" alt="Folder.png" class="img-fluid">
-            <p class="text-white" style="text-overflow: ellipsis"><small>{{ $directory->name ?? '' }}</small></p>
+            <p class="text-black" style="text-overflow: ellipsis"><small>{{ $directory->name ?? '' }}</small></p>
     </button>
     <ul class="dropdown-menu text-left">
         <li><a href="{{ route($route ?? 'archives-page') }}?directory={{ $directory->id }}" class="text-decoration-none px-2" ><i class="fa fa-folder"></i> Open Folder</a></li>
